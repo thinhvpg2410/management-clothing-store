15b065e7959702b1f4b20c865227b986c35d68ff
thinhvpg2410 <thinh.vpg@gmail.com>
2023-09-05 10:26:31 +0700

    [FE] Init JFrame

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import gui.GUI;
+
 public class Main {
     public static void main(String[] args) {
+        new GUI();
         System.out.println("Hello world!");
     }
 }
---
+++ src/gui/GUI.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.*;
+
+public class GUI {
+    public GUI() {
+        JFrame frame = new JFrame();
+        frame.setSize(600, 300);
+        frame.setTitle("HỆ THỐNG QUẢN LÝ CỬA HÀNG BÁN QUẦN ÁO");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
