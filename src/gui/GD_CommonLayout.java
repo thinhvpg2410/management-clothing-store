2c35518c30c668f3cf3f7539575197647f885be8
bobbibao <lehoangbao1125@gmail.com>
2023-11-07 05:14:22 +0700

    Add bill.png, clothes, customer.png, help.png, staff.png, statistics.png, supplier.png Icons into img/icon/ folder for CommonLayout

--- src/gui/GD_CommonLayout.java
+++ src/gui/GD_CommonLayout.java
@@ -12,10 +12,12 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 public class GD_CommonLayout extends JFrame {
 	private final int WIDTH = 1400;
@@ -33,39 +35,66 @@ public class GD_CommonLayout extends JFrame {
 		JPanel sideBar = sideBarUI();
 		JPanel content = contentUI();
 		
-		container.add(navigation, BorderLayout.NORTH);
+		content.add(navigation, BorderLayout.NORTH);
 		container.add(sideBar, BorderLayout.WEST);
-		container.add(sideBar, BorderLayout.CENTER);
+		container.add(content, BorderLayout.CENTER);
+		
 		Color backgroundColorContainer = Color.decode("#F5EFE0");
 		container.setBackground(backgroundColorContainer);
 		
 		container.add(sideBar, BorderLayout.WEST);
 		this.add(container);
 	}
+	
 	private JPanel navigationUI() {
 		JPanel nav = new JPanel();
-		nav.setPreferredSize(new Dimension(WIDTH - WIDTH/5, 100));
+		nav.setPreferredSize(new Dimension(WIDTH - WIDTH/7, 70));
 		nav.setBackground(new Color(0, 0, 0, 0));
 		return nav;
 	}
+	
 	private JPanel sideBarUI() {
 		JPanel sideBar = new JPanel(new BorderLayout());
-		sideBar.setPreferredSize(new Dimension(WIDTH/5, HEIGHT));
+		JPanel top = new JPanel();
+		JPanel center = new JPanel();
+		JPanel bottom = new JPanel();
+		Box boxContainer = Box.createVerticalBox();
+		sideBar.add(top, BorderLayout.NORTH);
+		sideBar.add(center, BorderLayout.CENTER);
+		sideBar.add(bottom, BorderLayout.SOUTH);
+		
+		top.setPreferredSize(new Dimension(WIDTH/7, HEIGHT/4));
+		bottom.setPreferredSize(new Dimension(WIDTH/7, HEIGHT/5));
+		sideBar.setPreferredSize(new Dimension(WIDTH/7, HEIGHT));
+		
 		Color backgroundColorSideBar = Color.decode("#CD2653");
 		sideBar.setBackground(backgroundColorSideBar);
+		top.setBackground(backgroundColorSideBar);
+		bottom.setBackground(backgroundColorSideBar);
+		boxContainer.setBackground(backgroundColorSideBar);
+		center.setBackground(backgroundColorSideBar);
+		String[] title = {"BÁN HÀNG (F1)", "KHÁCH HÀNG (F2)", "NHÂN VIÊN (F3)", "QUẦN ÁO (F4)"
+				+ "NHÀ CUNG CẤP (F5)", "THỐNG KÊ (F6)", "HOÁ ĐƠN (F7)", "HƯỚNG DẪN SỬ DỤNG (F8)"};
+		String rootPath = "img/icon/";
+		String[] filePath = {"shopping-cart.png"};
+		ImageIcon icon = new ImageIcon(createImage("img/icon/shopping-cart.png", 30, 30));
+		boxContainer.add(menuLabel);
+		center.add(boxContainer);
 		return sideBar;
 	}
+	
 	private JPanel contentUI() {
 		JPanel content = new JPanel(new BorderLayout());
-		content.setPreferredSize(new Dimension(WIDTH/5, HEIGHT));
 		content.setBackground(new Color(0, 0, 0, 0));
 		return content;
 	}
+	
+	//Additional Function
 	private JPanel createMenuLabel(ImageIcon icon, String title) {
 		JPanel menuLabel = new JPanel();
 		JLabel iconLabel = new JLabel(icon);
-		JLabel titleLabel = new JLabel(title);
-		
+		JLabel titleLabel = new JLabel("<html><b style='color: #ffffff; front-size: 22'>" + title + "</b></html>");
+		titleLabel.setPreferredSize(new Dimension(WIDTH/9, 30));
 		menuLabel.setBackground(new Color(0, 0, 0, 0));
 		menuLabel.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.WHITE));
 
