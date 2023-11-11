71fd48e9ef8d983cdd247dc012586254e5d95a01
bobbibao <lehoangbao1125@gmail.com>
2023-11-12 01:03:59 +0700

    GD_SanPham: Add toolbar UI consist of filter, sort and search with demo

--- src/gui/GD_SanPham.java
+++ src/gui/GD_SanPham.java
@@ -11,13 +11,21 @@ import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ListCellRenderer;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
@@ -35,7 +43,7 @@ public class GD_SanPham extends GD_CommonLayout {
     @Override
     public JPanel contentUI() {
         JPanel content = new JPanel(new BorderLayout());
-        JPanel toolbar = new JPanel();
+        JPanel toolbar = new JPanel(new GridLayout(1, 2, 50, 20));
         JPanel productList = new JPanel(new GridLayout(2, 4, 90, 40));
         JPanel paginationContainer = new JPanel();
 
@@ -54,14 +62,83 @@ public class GD_SanPham extends GD_CommonLayout {
         }
         
         JPanel pag = paginationUI();
+        JPanel toolLeft = toolLeftUI();
+        JPanel toolRight = toolRightUI();
         paginationContainer.add(pag);
         paginationContainer.setBorder(BorderFactory.createEmptyBorder(5, 10, 10, 10));
-        toolbar.setPreferredSize(new Dimension(getHeight(), 50));
+        toolbar.setPreferredSize(new Dimension(getWidth(), 60));
+        toolbar.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
+        toolbar.add(toolLeft);
+        toolbar.add(toolRight);
         paginationContainer.setPreferredSize(new Dimension(getHeight(), 80));
         productList.setBorder(BorderFactory.createEmptyBorder(20, 40, 20, 40));
         return content;
     }
+    public JPanel toolLeftUI() {
+    	JPanel leftTool = new JPanel(new GridLayout(1, 3, 40, 20));
+    	String[] filter = {"LỌC", "Size", "Ngừng Bán"};
+    	String[] sorted = {"SẮP XẾP", "Tên", "Giá"};
+    	JComboBox<String> CBfilter = new JComboBox<String>(filter);
+    	JComboBox<String> CBsorted = new JComboBox<String>(sorted);
+//    	setComboBoxIcon(CBfilter, "img/icon/filter.png");
+//        setComboBoxIcon(CBsorted, "img/icon/sorted.png");
+    	leftTool.add(CBfilter);
+    	leftTool.add(CBsorted);
+    	leftTool.add(Box.createHorizontalStrut(10));
+    	leftTool.setBackground(Color.decode("#F8E2E2"));
+    	return leftTool;
+    }
+//    public void setComboBoxIcon(JComboBox<String> comboBox, String iconPath) {
+//        ImageIcon icon = new ImageIcon(createImage(iconPath, 20, 20));
+//        comboBox.setRenderer(new DefaultListCellRenderer() {
+//        	 @Override
+//             public Component getListCellRendererComponent(JList<?> list, Object value, int index,
+//                                                           boolean isSelected, boolean cellHasFocus) {
+//                 JPanel panel = new JPanel(new BorderLayout());
+//                 JLabel label = new JLabel(value.toString());
+//                 label.setIcon(icon);
+//                 panel.add(label, BorderLayout.CENTER);
+//                 return panel;
+//             }
+//        });
+//    }
+    public JPanel toolRightUI() {
+    	JPanel rightTool = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Dimension arcs = new Dimension(40, 40);
+                int width = getWidth();
+                int height = getHeight();
+                Graphics2D graphics = (Graphics2D) g;
+                graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
+
+                //Draws the rounded opaque panel with borders.
+                graphics.setColor(Color.white);
+                graphics.fillRoundRect(0, 0, width - 1, height - 1, arcs.width, arcs.height);//paint background
+            }
+        };
+        Box box = Box.createHorizontalBox();
+    	JTextField searchInput = new JTextField(33);
+    	JPanel btn = new JPanel();
+    	JLabel text = new JLabel("TÌM KIẾM");
+    	ImageIcon searchIcon = new ImageIcon(createImage("img/icon/search.png", 20, 20));
+    	JLabel icon = new JLabel(searchIcon);
+    	box.add(searchInput);
+    	box.add(btn);
+    	btn.add(text);
+    	btn.add(icon);
+    	btn.setBackground(new Color(0, 0, 0, 0));
+    	btn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+    	btn.setBorder(BorderFactory.createMatteBorder(0, 1, 0, 0, Color.decode("#EDEDED")));
+    	rightTool.setBackground(new Color(0, 0, 0, 0));
+    	rightTool.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+    	searchInput.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.white));
+    	rightTool.add(box);
+    	return rightTool;
+    			
+    }
     public JPanel createProductCard() {
         JPanel productCard = new JPanel() {
             @Override
@@ -142,7 +219,6 @@ public class GD_SanPham extends GD_CommonLayout {
         productCard.add(boxCard);
         return productCard;
     }
-
     public JPanel paginationUI() {
     	JPanel pagCenter = new JPanel(new GridLayout(1, 6, 20, 0));
     	Box boxPag = Box.createHorizontalBox();
@@ -201,16 +277,33 @@ public class GD_SanPham extends GD_CommonLayout {
                  //Draws the rounded opaque panel with borders.
                  graphics.setColor(Color.white);
                  graphics.fillRoundRect(0, 0, width - 1, height - 1, arcs.width, arcs.height);//paint background
-                 graphics.setColor(Color.white);
-                 graphics.drawRoundRect(0, 0, width - 1, height - 1, arcs.width, arcs.height);//paint border
              }
     	};
+
     	node.setBackground(new Color(0, 0, 0, 0));
+    	node.setCursor(new Cursor(Cursor.HAND_CURSOR));
+    	node.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            	node.paintComponents(getGraphics());
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                try{
+            		lbl.setForeground(Color.decode("#6D6D6D"));
+            	}catch (Exception e2){
+            		
+            	}
+            }
+        });
+
     	node.setPreferredSize(new Dimension(35, 35));
     	node.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
     	node.add(lbl);
     	return node;
     }
+    
     public static void main(String[] args) {
         new GD_SanPham().setVisible(true);
     }
