411863951556da4e57577aa205c1098708198088
bobbibao <lehoangbao1125@gmail.com>
2023-11-11 14:27:51 +0700

    GD_SanPham: Add pagination UI

--- src/gui/GD_SanPham.java
+++ src/gui/GD_SanPham.java
@@ -25,10 +25,10 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
 
-public class GD_Product extends GD_CommonLayout {
+public class GD_SanPham extends GD_CommonLayout {
     private static final long serialVersionUID = 1L;
 
-    public GD_Product() {
+    public GD_SanPham() {
         super("QUẢN LÝ SẢN PHẨM");
     }
 
@@ -37,22 +37,27 @@ public class GD_Product extends GD_CommonLayout {
         JPanel content = new JPanel(new BorderLayout());
         JPanel toolbar = new JPanel();
         JPanel productList = new JPanel(new GridLayout(2, 4, 90, 40));
-        JPanel pagination = new JPanel();
+        JPanel paginationContainer = new JPanel();
 
         content.add(toolbar, BorderLayout.NORTH);
         content.add(productList, BorderLayout.CENTER);
-        content.add(pagination, BorderLayout.SOUTH);
+        content.add(paginationContainer, BorderLayout.SOUTH);
 
         content.setBackground(new Color(0, 0, 0, 0));
         productList.setBackground(new Color(0, 0, 0, 0));
+        paginationContainer.setBackground(new Color(0, 0, 0, 0));
         toolbar.setBackground(Color.decode("#F8E2E2"));
 
         for (int i = 0; i < 8; i++) {
             JPanel productCard = createProductCard();
             productList.add(productCard);
         }
+        
+        JPanel pag = paginationUI();
+        paginationContainer.add(pag);
+        paginationContainer.setBorder(BorderFactory.createEmptyBorder(5, 10, 10, 10));
         toolbar.setPreferredSize(new Dimension(getHeight(), 50));
-        pagination.setPreferredSize(new Dimension(getHeight(), 80));
+        paginationContainer.setPreferredSize(new Dimension(getHeight(), 80));
         productList.setBorder(BorderFactory.createEmptyBorder(20, 40, 20, 40));
         return content;
     }
@@ -138,8 +143,76 @@ public class GD_Product extends GD_CommonLayout {
         return productCard;
     }
 
+    public JPanel paginationUI() {
+    	JPanel pagCenter = new JPanel(new GridLayout(1, 6, 20, 0));
+    	Box boxPag = Box.createHorizontalBox();
+    	ImageIcon prev = new ImageIcon(createImage("img/icon/pag-prev.png", 15, 15));
+    	ImageIcon next = new ImageIcon(createImage("img/icon/pag-next.png", 15, 15));
+    	JPanel pag = new JPanel() {
+    		@Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Dimension arcs = new Dimension(52, 52);
+                int width = getWidth();
+                int height = getHeight();
+                Graphics2D graphics = (Graphics2D) g;
+                graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+
+                //Draws the rounded opaque panel with borders.
+                graphics.setColor(Color.decode("#F8E2E2"));
+                graphics.fillRoundRect(0, 0, width - 1, height - 1, arcs.width, arcs.height);//paint background
+//                graphics.setColor(Color.white);
+//                graphics.drawRoundRect(0, 0, width - 1, height - 1, arcs.width, arcs.height);//paint border
+            }
+    	};
+    	pag.setBorder(BorderFactory.createEmptyBorder(2, 25, 2, 25));
+    	for(int i = 1; i <= 6; i++) {
+    		pagCenter.add(createNodePag(new JLabel(""+i)));
+    	}
+    	pagCenter.setBackground(new Color(0, 0, 0, 0));
+    	boxPag.add(createNodePag(new JLabel(prev)));
+    	boxPag.add(Box.createHorizontalStrut(30));
+    	boxPag.add(pagCenter);
+    	boxPag.add(Box.createHorizontalStrut(30));
+    	boxPag.add(createNodePag(new JLabel(next)));
+    	
+    	pag.add(boxPag);
+    	return pag;
+    }
+    public JPanel createNodePag(JLabel lbl) {
+    	lbl.setFont(new Font(Font.MONOSPACED, 1, 13));
+    	try{
+    		lbl.setForeground(Color.decode("#6D6D6D"));
+    	}catch (Exception e){
+    		
+    	}
+    	JPanel node = new JPanel() {
+    		 @Override
+             protected void paintComponent(Graphics g) {
+                 super.paintComponent(g);
+                 Dimension arcs = new Dimension(35, 38);
+                 int width = getWidth();
+                 int height = getHeight();
+                 Graphics2D graphics = (Graphics2D) g;
+                 graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+
+                 //Draws the rounded opaque panel with borders.
+                 graphics.setColor(Color.white);
+                 graphics.fillRoundRect(0, 0, width - 1, height - 1, arcs.width, arcs.height);//paint background
+                 graphics.setColor(Color.white);
+                 graphics.drawRoundRect(0, 0, width - 1, height - 1, arcs.width, arcs.height);//paint border
+             }
+    	};
+    	node.setBackground(new Color(0, 0, 0, 0));
+    	node.setPreferredSize(new Dimension(35, 35));
+    	node.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
+    	node.add(lbl);
+    	return node;
+    }
     public static void main(String[] args) {
-        new GD_Product().setVisible(true);
+        new GD_SanPham().setVisible(true);
     }
 
 }
